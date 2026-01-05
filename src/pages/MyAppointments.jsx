2e07db89e91600af24a204c64d69142dc57020b7
fadqus <fadqus2005@gmail.com>
2026-01-05 18:05:08 +0100

    update appointment page

--- src/pages/MyAppointments.jsx
+++ src/pages/MyAppointments.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import {AppContext} from '../context/AppContext'
+import { assets } from '../assets/assets'
 
 const MyAppointments = () => {
+
+  const {doctors} = useContext(AppContext)
+
   return (
     <div>
-        
+     <p className='font-3xl'>My appointments</p>
+     <div>
+        {doctors.slice(0,2).map((item,index)=>(
+          <div key={index}>
+            <div>
+              <img src={assets.about_image} alt="" />
+            </div>
+          </div>
+        ))}
+     </div>
     </div>
   )
 }
